4ff7387f1258e52c54fd2d0e9fd378e8542d4d05
ikeepo <ikeepo@proton.me>
2024-12-07 23:12:45 +0800

    u

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,41 +1,41 @@
-import Taro from '@tarojs/taro';
-import {useState} from 'react'
-import {RichText, Text, Image, View} from '@tarojs/components';
-import { Input, Button } from '@nutui/nutui-react-taro'
-import {getMatchId} from '@/http/api';
-import useShareApp from '@/hooks/useShareApp';
+import Taro from "@tarojs/taro";
+import { useState } from "react";
+import { RichText, Text, Image, View } from "@tarojs/components";
+import { Input, Button } from "@nutui/nutui-react-taro";
+import { getMatchId } from "@/http/api";
+import useShareApp from "@/hooks/useShareApp";
 
-import styles from './index.module.less'
-import logo from '../../assets/logo.png'
+import styles from "./index.module.less";
+import logo from "../../assets/logo.png";
 
 export default function Home() {
-  useShareApp()
-  const [code, setCode] = useState('')
+  useShareApp();
+  const [code, setCode] = useState("");
   const prediction = async () => {
     if (code.length === 0) {
       Taro.showToast({
-        title: '请输入赛事编号',
-        icon: 'none'
-      })
-      return
+        title: "请输入赛事编号",
+        icon: "none"
+      });
+      return;
     }
-    const res = await getMatchId({code})
+    const res = await getMatchId({ code });
     if (res?.data?.data?.matchId) {
-      setCode('')
+      setCode("");
       Taro.navigateTo({
-        url: '/pages/home/detail/index?id=' + res?.data?.data?.matchId
-      })
+        url: "/pages/home/detail/index?id=" + res?.data?.data?.matchId
+      });
     } else {
       Taro.showToast({
-        icon: 'none',
-        title: '当前无可查赛事详情'
-      })
+        icon: "none",
+        title: "当前无可查赛事详情"
+      });
     }
-  }
+  };
   return (
     <View className={styles.home}>
       <View className={styles.home__logo}>
-        <Image className={styles.img__logo} mode='aspectFit' src={logo} />
+        <Image className={styles.img__logo} mode="aspectFit" src={logo} />
         {/*<Video*/}
         {/*  className={styles.img__logo}*/}
         {/*  src={`${process.env.TARO_APP_BASEURL}/images/1.mp4`}*/}
@@ -54,19 +54,21 @@ export default function Home() {
       </View>
       <View className={styles.search__form}>
         <div className={styles.search__input}>
-          <Input type='number' style={{'--nutui-input-color': '#333'}}
+          <Input
+            type="number"
+            style={{ "--nutui-input-color": "#333" }}
             className={styles.input}
-            placeholder='输入竞彩赛事编号如025'
+            placeholder="输入竞彩赛事编号如025"
             value={code}
             onChange={val => setCode(val)}
           />
           <Button className={styles.search__button} onClick={prediction}>
-            <RichText nodes='预测<br />一下'></RichText>
+            <RichText nodes="预测<br />一下"></RichText>
           </Button>
         </div>
         <Text>*本次推荐仅作测试参考之用，并不构成购彩建议。</Text>
         <Text>一切互联网彩票均属违法，请到线下彩站购买。</Text>
       </View>
     </View>
-  )
+  );
 }
--- tutorials/general.md
+++ tutorials/general.md
@@ -1,11 +1,42 @@
 # question 10
+
 ### why pnpm
-一般是通过pnpm-lock.yaml判断，此项目使用的是pnpm。  
-pnpm的同位置替代是npm yarn.
-### JS版本React vs TS版本React
-并不存在这个说法，react本身是用JavaScript写的，但是TypeScript作为JS的一个超集扩展，也可以使用react，但是需要对js的react进行封装扩展。  
+
+一般是通过 pnpm-lock.yaml 判断，此项目使用的是 pnpm。  
+pnpm 的同位置替代是 npm yarn.
+
+### JS 版本 React vs TS 版本 React
+
+并不存在这个说法，react 本身是用 JavaScript 写的，但是 TypeScript 作为 JS 的一个超集扩展，也可以使用 react，但是需要对 js 的 react 进行封装扩展。  
 这个封装扩展是用[@types/react](https://github.com/DefinitelyTyped/DefinitelyTyped)实现的。  
-@types/react在做封装的时候，是以类型为接口，但是不仅仅是类型，还会做一定的功能扩展，以增加原始模块的使用便捷性、安全性。
-### Taro API自己也有界面组件，为什么还需要Taro-UI/NutUI这种专门的UI组件库
-Taro API是功能方法，不是组件；   
-Taro也的确带有内置组件，但都是只提供最基础的组件，保证的是跨端兼容，但是不够丰富；   
+@types/react 在做封装的时候，是以类型为接口，但是不仅仅是类型，还会做一定的功能扩展，以增加原始模块的使用便捷性、安全性。
+
+### Taro API 自己也有界面组件，为什么还需要 Taro-UI/NutUI 这种专门的 UI 组件库
+
+Taro API 是功能方法，不是组件；  
+Taro 也的确带有内置组件，但都是只提供最基础的组件，保证的是跨端兼容，但是不够丰富；
+
+### Issues with peer dependencies found
+
+```shell
+ WARN  Issues with peer dependencies found
+.
+├─┬ @tarojs/react 3.6.35
+│ ├── ✕ unmet peer @tarojs/runtime@~3.6.35: found 3.6.24
+│ └── ✕ unmet peer @tarojs/shared@~3.6.35: found 3.6.24
+├─┬ @tarojs/webpack5-runner 3.6.24
+│ └─┬ vue-loader 15.11.1
+│   └─┬ @vue/component-compiler-utils 3.3.0
+│     └─┬ consolidate 0.15.1
+│       ├── ✕ unmet peer react-dom@^16.13.1: found 18.3.1
+│       └── ✕ unmet peer react@^16.13.1: found 18.3.1
+├─┬ @tarojs/plugin-html 3.6.35
+│ └─┬ @tarojs/service 3.6.35
+│   └── ✕ unmet peer @tarojs/taro@~3.6.35: found 3.6.24
+└─┬ @tarojs/redux-h5 2.2.10
+  └── ✕ unmet peer redux@"^2.0.0 || ^3.0.0 || ^4.0.0-0": found 5.0.1
+```
+
+```
+
+```
